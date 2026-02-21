67f88aa713e7ca609786811fd8cbfca3d18a875b
Raja Sarkar <rajasarkar8167@gmail.com>
2026-02-21 23:55:57 +0530

    Adding models

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Package, Booking, Review
 
 # Register your models here.
+admin.site.register(Package)
+admin.site.register(Booking)
+admin.site.register(Review)
--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     phone = models.CharField(max_length=15)
+#     profile_image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+
+class Package(models.Model):
+    title = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 50)
+    price = models.IntegerField()
+    duration = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 150)
+    image = models.ImageField(upload_to = 'packages/')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    package = models.ForeignKey(Package, on_delete = models.CASCADE)
+    booking_date = models.DateField()
+    persons = models.IntegerField()
+    total_price = models.IntegerField()
+    status = models.CharField(max_length = 20, default = 'pending')
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    package = models.ForeignKey(Package, on_delete = models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+
+
