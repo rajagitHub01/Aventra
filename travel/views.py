a61f83be01700f11c8957bf2fe9dda9752946254
Raja Sarkar <rajasarkar8167@gmail.com>
2026-02-24 23:24:40 +0530

    add package details

--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Package
 
 # Create your views here.
+
+def home(request):
+    packages = Package.objects.all()
+    return render(request, 'index.html', {'packages': packages})
+
+def package_detail(request, id):
+    package = get_object_or_404(Package, id=id)
+    return render(request, 'package_detail.html', {'package': package})
